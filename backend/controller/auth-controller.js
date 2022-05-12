5af08cb65f19b74dfe9b341009c0b8fe076b523a
fadouakharroub <76811834+fadouakharroub@users.noreply.github.com>
2022-05-12 22:57:20 +0100

    'Last commit'

--- backend/controller/auth-controller.js
+++ backend/controller/auth-controller.js
@@ -1,6 +1,7 @@
 const { userModel } = require("../model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+// const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   try {
